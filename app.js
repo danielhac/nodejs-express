c10ec03977d469646508b9c488b5c9e0929b009f
Dan <danhboi23@hotmail.com>
2016-12-28 00:13:29 -0800

    Wired up ExpressJs & node folder

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
 
-var hi = 'hola';
+var app = express();
 
-if (true) {
-    console.log(hi);
-}
+var port = 5001;
 
+app.get('/', function(req, res) {
+    res.send('Yo World');
+});
+
+app.get('/books', function(req, res) {
+    res.send('Yo books');
+});
+
+app.listen(port, function(err) {
+    console.log('Running server on port ' + port);
+});
