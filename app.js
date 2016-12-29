adf88422a4dbfd2dd73a4ce37b8db87eda036716
Dan <danhboi23@hotmail.com>
2016-12-29 14:31:11 -0800

    Added rendering and books.ejs

--- app.js
+++ app.js
@@ -13,9 +13,34 @@ app.set('views', './src/views');
 // EJS - Template engine
 app.set('view engine', 'ejs');
 
+var books = [
+    {
+        title: 'Testing title 1',
+        genre: 'I am testing genre 1',
+        author: 'Again testing author 1',
+        read: false
+    },
+    {
+        title: 'Testing title 2',
+        genre: 'I am testing genre 2',
+        author: 'Again testing author 2',
+        read: false
+    }
+];
+
 bookRouter.route('/')
     .get(function(req, res) {
-        res.send('Hello Books');
+        res.render('books', {
+            title: 'Books',
+            nav: [{
+                Link: '/Books',
+                Text: 'Books'
+            }, {
+                Link: '/Authors',
+                Text: 'Authors'
+            }],
+            books: books
+        });
     });
 
 bookRouter.route('/single')
