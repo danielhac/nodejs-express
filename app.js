2b7e3986ab6347f244c8a378e98091f01b30a2e5
Dan <danhboi23@hotmail.com>
2016-12-29 13:59:34 -0800

    Added simple routing

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5001;
+var bookRouter = express.Router();
 
 // Set up middleware to be used by Express
 // Looks in 'public' directory for the related css/js static files (styles.css, etc), then it starts the routes
@@ -11,6 +12,18 @@ app.set('views', './src/views');
 
 // EJS - Template engine
 app.set('view engine', 'ejs');
+
+bookRouter.route('/')
+    .get(function(req, res) {
+        res.send('Hello Books');
+    });
+
+bookRouter.route('/single')
+    .get(function(req, res) {
+        res.send('Hello Single Book');
+    });
+
+app.use('/Books', bookRouter);
 
 app.get('/', function(req, res) {
     res.render('index', {
