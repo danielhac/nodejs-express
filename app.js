c6a97bb0e8c2fab2a6d8405fe48535f2efc02b58
Dan <danhboi23@hotmail.com>
2016-12-29 13:47:06 -0800

    Wired up navigation

--- app.js
+++ app.js
@@ -13,7 +13,16 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('index', {title: 'Yo from render', list: ['a','b']});
+    res.render('index', {
+        title: 'Yo from render',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    });
 });
 
 app.get('/books', function(req, res) {
