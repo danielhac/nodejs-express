a3215a91c306ccee32dfc181753f400e1c91dd1f
Dan <danhboi23@hotmail.com>
2016-12-30 00:35:21 -0800

    Connected MongoDb in bookRoutes.js

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,29 +1,25 @@
 var express = require('express');
-
 var bookRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
 
 var router = function(nav) {
-    var books = [
-        {
-            title: 'Testing title 1',
-            genre: 'I am testing genre 1',
-            author: 'Again testing author 1',
-            read: false
-        },
-        {
-            title: 'Testing title 2',
-            genre: 'I am testing genre 2',
-            author: 'Again testing author 2',
-            read: false
-        }
-    ];
 
     bookRouter.route('/')
         .get(function(req, res) {
-            res.render('bookListView', {
-                title: 'Books',
-                nav: nav,
-                books: books
+            var url = 'mongodb://localhost:27017/libraryApp';
+
+            mongodb.connect(url, function(err, db) {
+                var collection = db.collection('books');
+
+                collection.find({}).toArray(
+                    function(err, results) {
+                        res.render('bookListView', {
+                            title: 'Books',
+                            nav: nav,
+                            books: results
+                        });
+                    }
+                );
             });
         });
 
