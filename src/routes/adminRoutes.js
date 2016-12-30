77ce86f0331f9a38da61364d28d1ee75e34b2631
Dan <danhboi23@hotmail.com>
2016-12-29 23:12:35 -0800

    Set up MongoDB and books json data within adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,11 +1,38 @@
 var express = require('express');
 var adminRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+
+var books = [
+    {
+        title: 'Testing title 1',
+        genre: 'I am testing genre 1',
+        author: 'Again testing author 1',
+        read: false
+    },
+    {
+        title: 'Testing title 2',
+        genre: 'I am testing genre 2',
+        author: 'Again testing author 2',
+        read: false
+    }
+];
 
 var router = function(nav) {
 
     adminRouter.route('/addBooks')
         .get(function(req, res) {
-            res.send('insert books');
+            var url = 'mongodb://localhost:27017/libraryApp';
+
+            mongodb.connect(url, function(err, db) {
+                var collection = db.collection('books');
+                collection.insertMany(books,
+                    function(err, results) {
+                        res.send(results);
+                        db.close();
+                    }
+                );
+            });
+
         });
 
     return adminRouter;
