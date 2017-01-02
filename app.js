cc05ce31b9db7724267538ede0ca819db9d84302
Dan <danhboi23@hotmail.com>
2017-01-01 18:33:43 -0800

    Created authRoutes.js and wired it up in app.js with body-parser

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -14,10 +15,15 @@ var nav = [{
 
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 // Set up middleware to be used by Express
 // Looks in 'public' directory for the related css/js static files (styles.css, etc), then it starts the routes
 app.use(express.static('public'));
+// Check if body coming in is JSON, then it will parse it out and will create a req.body object
+app.use(bodyParser.json());
+// Same as above but for url encoded bodies
+app.use(bodyParser.urlencoded());
 app.set('views', './src/views');
 
 // EJS - Template engine
@@ -25,6 +31,7 @@ app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 app.get('/', function(req, res) {
     res.render('index', {
